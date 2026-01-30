19dc7a9ed959d345fdb00b09b40235e986e8d3e3
Adam Barański <baranski2023@gmail.com>
2026-01-30 01:33:43 +0100

    Added player movement animations
    Implemented directional animations for the player character in player.gd and updated player.tscn to use an AnimatedSprite2D with multiple animation frames. Switched the player texture to a tileset and configured animation frames for up, down, left, and right movement.

--- player.gd
+++ player.gd
@@ -6,4 +6,18 @@ var speed: int = 140
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("left","right","up","down")
 	velocity = direction * speed
+	animation()
 	move_and_slide()
+	
+func animation():
+	if direction:
+		if direction.x > 0:
+			$AnimatedSprite2D.play("right")
+		elif direction.x < 0:
+			$AnimatedSprite2D.play("left")
+		elif direction.y <0:
+			$AnimatedSprite2D.play("up")
+		else:
+			$AnimatedSprite2D.play("down")
+	else:
+		$AnimatedSprite2D.frame = 0
