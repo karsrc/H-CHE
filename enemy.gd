8f387ee670c0ab5f7b33287c1393944ac1e50f3a
Adam Barański <baranski2023@gmail.com>
2026-01-30 03:26:51 +0100

    Improve enemy movement and collision shape
    Enhanced enemy movement logic to maintain distance from the player and added aggro/ok range parameters. Updated enemy's collision shape from circle to rectangle and adjusted its position. Also moved Enemy1 node to the main scene hierarchy in node_2d.tscn.

--- enemy.gd
+++ enemy.gd
@@ -1,14 +1,35 @@
 extends CharacterBody2D
 
-@export var speed = 20.0
+@export var speed = 85.0
+@export var aggro_range = 600
+@export var ok_range = 15
 @onready var player = get_tree().get_first_node_in_group("player")
 
-func _physics_process(_delta):
+
+var target_velocity = Vector2.ZERO
+
+func _ready():
 	if player == null:
 		return
-		
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * speed
+	
+	
+	await get_tree().process_frame
+	player = get_tree().get_first_node_in_group("player")
+	
+
+func _physics_process(_delta):
+	var offset = player.global_position - global_position
+	var direction = offset.normalized()
+	var distance = offset.length()
+	
+	var target_velocity = Vector2.ZERO
+	
+	if distance < aggro_range:
+		var desired_position = player.global_position - direction * ok_range
+		var desired_direction = (desired_position - global_position).normalized()
+
+		target_velocity = desired_direction * speed
+		velocity = velocity.lerp(target_velocity, 0.15)
 	move_and_slide()
 	animation()   
 
