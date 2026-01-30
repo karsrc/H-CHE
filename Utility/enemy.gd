7baef3dc27df594955c68bfe84b54a30a73db7c2
Adam Barański <baranski2023@gmail.com>
2026-01-30 21:38:53 +0100

    Add enemy spawner system and adjust enemy speed
    Introduced an enemy spawner with configurable spawn info resources and scene integration. Removed static enemy from main scene and reduced enemy speed for better gameplay balance.

--- Utility/enemy.gd
+++ Utility/enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 85.0
+@export var speed = 35.0
 @export var aggro_range = 600
 @export var hp = 10
 @onready var player = get_tree().get_first_node_in_group("player")
